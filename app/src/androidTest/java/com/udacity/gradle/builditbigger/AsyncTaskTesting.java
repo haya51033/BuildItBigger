200544858e94aa12a7cb6490a70cf665b9718518
haya <haya.akkad94@gmail.com>
2018-06-23 11:21:22 +0300

    Finish Testing

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTesting.java
@@ -0,0 +1,50 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v4.util.Pair;
+import android.test.InstrumentationTestCase;
+
+import org.mockito.Mockito;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.Mockito.verify;
+
+public class AsyncTaskTesting extends InstrumentationTestCase{
+
+
+    public void testVerifyEchoResponse() throws Throwable {
+
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        final AsyncTask<Pair<Context, String>, Void, String> AsyncTask =
+                new AsyncTask<Pair<Context, String>, Void, String>(){
+
+                    @Override
+                    protected String doInBackground(Pair<Context, String>... pairs) {
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(String result) {
+                        super.onPostExecute(result);
+
+                        latch.countDown();
+                    }
+                };
+
+        runTestOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                AsyncTask.execute();
+            }
+        });
+
+        latch.await(30, TimeUnit.SECONDS);
+
+        assertTrue("Testing Done!", true);
+    }
+}
